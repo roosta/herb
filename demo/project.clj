c75efc9bac1d0d40f1c40b22c17f5dabb11139a2
Daniel Berg <mail@roosta.sh>
2018-01-20 20:49:44 +0100

    Add demo

---
+++ demo/project.clj
@@ -0,0 +1,46 @@
+(defproject demo "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/clojurescript "1.9.946"]
+                 [reagent "0.8.0-alpha2"]]
+
+  :plugins [[lein-cljsbuild "1.1.7"]
+            [lein-figwheel "0.5.15-SNAPSHOT"]]
+
+  :min-lein-version "2.5.0"
+
+  :clean-targets ^{:protect false}
+  [:target-path
+   [:cljsbuild :builds :app :compiler :output-dir]
+   [:cljsbuild :builds :app :compiler :output-to]]
+
+  :resource-paths ["public"]
+
+  :figwheel {:http-server-root "."
+             :nrepl-port 7002
+             :nrepl-middleware ["cemerick.piggieback/wrap-cljs-repl"]
+             :css-dirs ["public/css"]}
+
+  :cljsbuild {:builds {:app
+                       {:source-paths ["src" "../src"]
+                        :compiler
+                        {:main "demo.core"
+                         :output-to "public/js/app.js"
+                         :output-dir "public/js/out"
+                         :asset-path   "js/out"
+                         :source-map true
+                         :optimizations :none
+                         :pretty-print  true}
+                        :figwheel
+                        {:on-jsload "demo.core/mount-root" }}}}
+
+  :aliases {"package" ["do" "clean" ["cljsbuild" "once" "release"]]}
+
+  :profiles {:dev {:dependencies [[binaryage/devtools "0.9.9"]
+                                  [figwheel-sidecar "0.5.15-SNAPSHOT"]
+                                  [org.clojure/tools.nrepl "0.2.13"]
+                                  [com.cemerick/piggieback "0.2.2"]]}})
