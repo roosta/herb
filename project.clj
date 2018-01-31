0a37751d03e15f0ed606e690049586da18df16c9
Daniel Berg <mail@roosta.sh>
2018-01-31 10:55:07 +0100

    Bump project version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject herb "0.1.0-SNAPSHOT"
+(defproject herb "0.2.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
