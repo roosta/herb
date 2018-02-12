2f14016c8452a735d797567cafafa7edafa7b560
Daniel Berg <mail@roosta.sh>
2018-02-12 20:25:46 +0100

    Add tests for convert-media/modes, resolve-styles

--- test/herb/core_test.cljs
+++ test/herb/core_test.cljs
@@ -1,6 +1,29 @@
 (ns herb.core-test
-  (:require [cljs.test :as t :refer-macros [deftest testing is]]))
+  (:require [cljs.test :as t :refer-macros [deftest testing is]]
+            [garden.units :refer [px]]
+            [garden.stylesheet :refer [at-media at-keyframes]]
+            [herb.core :as core]))
 
-(deftest a-test
-  (testing "FIXME: I pass"
-    (is (= 1 1))))
+(deftest convert-media
+  (let [input {{:screen true} {:color "white"}
+               {:min-width "100px"} {:color "blue"}}
+        expected (list (at-media {:screen true} [:& {:color "white"}])
+                       (at-media {:min-width "100px"} [:& {:color "blue"}]))]
+    (testing "Converting media queries"
+      (is (= (core/convert-media input) expected)))))
+
+(deftest convert-modes
+  (let [input {:hover {:opacity 1}
+               :focus {:color "yellow"}}
+        expected (list [:&:hover {:opacity 1}] [:&:focus {:color "yellow"}])]
+    (testing "Converting modes"
+      (is (= (core/convert-modes input) expected)))))
+
+(deftest resolve-styles
+  (letfn [(button []
+            {:border-radius "5px"})
+          (box [color] {:background-color color})]
+    (let [expected [{:background-color "green"} {:border-radius "5px"}]]
+      (testing "Resolve styles"
+        (is (= (core/resolve-styles [button [box "green"]])
+               expected))))))
