f4bbc922ac216e115f1d8351c4bffe6a333fdf6e
Daniel Berg <mail@roosta.sh>
2018-02-26 20:39:26 +0100

    Clear out old demo, add new head, container

--- demo/src/demo/core.cljs
+++ demo/src/demo/core.cljs
@@ -1,171 +1,72 @@
 (ns demo.core
   (:require
-   [herb.core :refer-macros [with-style]]
+   [herb.core :refer-macros [with-style] :as herb]
    [garden.selectors :as s]
    [garden.core :refer [css]]
    [garden.stylesheet :refer [at-media]]
    [taoensso.tufte :as tufte :refer-macros [defnp p profiled profile]]
    [garden.core :refer [css]]
-   [garden.units :refer [px]]
+   [garden.units :refer [rem em px]]
    [reagent.debug :as d]
    [reagent.core :as r]))
 
 (tufte/add-basic-println-handler! {})
 (enable-console-print!)
 
-(def red {:color "red"})
-(def green {:color "green"})
-
-(defn state-hover
-  [color]
-  ^{:mode {:hover {:color "yellow"}}}
-  {:margin-bottom 0
-   :background-color color})
-
-(defn toggle-color
-  [color]
-  (case color
-    "red" "green"
-    "green" "red"))
-
-(defn asd
+(defn container-style
   []
-  ^{:mode {:hover {:background-color "black"}}}
-  {:color "red"})
+  ^{:media {{:screen :only :min-width (em 48)} {:width (rem 49)}
+            {:screen :only :min-width (em 64)} {:width (rem 65)}
+            {:screen :only :min-width (em 75)} {:width (rem 76)}}}
+  {:margin-right "auto"
+   :margin-left "auto"})
 
-(defn extend-mode-meta
+(defn paper
   []
-  ^{:extend asd
-    :mode {:hover {:background-color "green"}}}
-  {:color "purple"})
-
-(defn hover-focus
-  []
-  ^{:extend extend-mode-meta
-    :mode {:hover {:background-color "blue"}
-           :focus {:background-color "yellow"}}}
-  {:background-color "red"}
   )
 
-(defn font-weight
-  [weight]
-  {:font-weight weight})
-
-(defn dynamic-text-color
-  [color]
-  (with-meta
-    (case color
-      (or "blue" "purple") {:color "white"}
-      {:color "black"})
-    {:extend [[font-weight "bold"] [asd]]}))
-
-(defn margin
+(defn container
   []
-  {:margin "5px"})
+  (into [:div {:class (with-style container-style)}]
+        (r/children (r/current-component))))
 
-(defn italic
+(def global-style
+  (list [:body {:font-family ["Helvetica Neue" "Verdana" "Helvetica" "Arial" "sans-serif"]}]
+        [:a {:text-decoration "none"
+             :color "#09f"}]))
+
+(defn flex-container
   []
-  ^{:mode {:hover {:font-style "normal"}}}
-  {:font-style "italic"})
+  {:display "flex"
+   :justify-content "center"})
 
-(defn bold
+(defn header-style
+  [component]
+  (let [styles {:container {:display "inline-block"}
+                :heading
+                {:font-size  (px 112)
+                 :margin-bottom 0
+                 :color "#333"}
+                :box
+                {:height (px 80)
+                 :display "flex"
+                 :justify-content "center"
+                 :align-items "center"
+                 :text-transform "uppercase"
+                 :background "#333"
+                 :font-size (px 56)
+                 :color "white"}
+                }]
+    (with-meta (component styles) {:key component})))
+
+(defn header
   []
-  ^{:extend [[italic] [margin]]}
-  {:font-weight "bold"})
-
-(defn cycle-color
-  [color]
-  ^{:key color
-    :extend [[dynamic-text-color color] [bold]]}
-  {:text-align "center"
-   :background-color color}
-  )
-
-(defn button
-  []
-  {:border "2px solid red"
-   :margin (px 10)})
-
-(defn blue-div
-  []
-  {:color "black"
-   :background-color "blue"})
-
-(defn red-div
-  []
-  ^{:extend blue-div
-    :media {{:screen true} {:color "magenta"}}}
-  {:color "white"
-   :background-color "red"})
-
-(defn cyan-div
-  []
-  ^{:extend red-div
-    :media {{:screen true} {:color "black"
-                            :background-color "purple"}}}
-  {:background-color "cyan"})
-
-(defn media-query-test
-  []
-  ^{:extend cyan-div
-    :media {{:screen true} {:color "white"
-                            :background-color "green"}
-            {:max-width "800px"} {:background-color "yellow"}}}
-  {:background "magenta"
-   :text-align "center"
-   :color "white"})
-
-(defn keyed
-  [k]
-  (let [styles {:paper
-                {:background-color "grey"
-                 :width (px 50)
-                 :display "inline-block"
-                 :margin (px 10)
-                 :height (px 50)
-                 :border-radius (px 5)}
-                :sheet
-                ^{:mode {:active {:color "white"}
-                         :hover {:border-radius (px 5)}}}
-                {:background-color "white"
-                 :width (px 50)
-                 :margin (px 10)
-                 :display "inline-block"
-                 :height (px 50)
-                 :box-shadow "0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23)"}}]
-    (vary-meta
-     (k styles)
-     assoc :key (name k))))
-
-(defn profile-comp
-  [n]
-  ^{:key n}
-  {:width (px 100)
-   :height (px 100)
-   :background-color "magenta"
-   :border-radius "5px"})
-
-(defn tmp-1
-  [color]
-  ^{:key color}
-  {:background-color color})
-
-(defn tmp-2
-  [color]
-  ^{:key color
-    :extend [tmp-1 "green"]}
-  {:color color})
-
-(defn tmp-3
-  []
-  ^{:extend [tmp-2 "red"]}
-  {:color "yellow"})
-
-;; (def some-other-style {:background-color "black"})
-;; (def some-more-style {:border-radius "5px"})
-;; (def some-style
-;;   ^{:extend [some-other-style some-more-style]}
-;;   {:color "blue"})
+  [:div {:class (with-style header-style :container)}
+   [:h4 {:class (with-style header-style :heading)}
+    "Herb demo"]
+   [:div {:class (with-style header-style :box)}
+    [:div
+     "styling library"]]])
 
 (defn home-page []
   (let [state (r/atom "green")]
@@ -189,56 +90,24 @@
            (p ::at-media (at-media {:max-width "256px"} [:.classname {:width (px 100)
                                                                       :height (px 100)
                                                                       :background-color "magenta"
-                                                                      :border-radius "5px"}])))
-         )
+                                                                      :border-radius "5px"}]))))
 
-      [:div
-       [:div {:class (with-style tmp-3)}
-        "hello"]
-
-       [:div
-
-        [:div
-         [:div {:class (with-style (fn []
-                                     ^{:extend button}
-                                     {:background-color "black"
-                                      :color "white"}))}
-          "Anon function with meta"]
-         [:div {:class (with-style (fn [color]
-                                     {:background-color color
-                                      :color "white"})
-                         "black")}
-          "Anon function without meta"]]
-
-        [:input {:class (with-style hover-focus)
-                 :default-value "Hello world"}]
-        [:div
-         [:h2 {:class (with-style state-hover @state)}
-          "Welcome to Reagent"]
-         [:button {:class (with-style button)
-                   :on-click #(reset! state (toggle-color @state))}
-          "Toggle"]
-         (for [c ["yellow" "blue" "green" "purple"]]
-           ^{:key c}
-           [:div {:class (with-style cycle-color c)}
-            c])
-         [:br]
-         [:div {:class (with-style cyan-div)}
-          "inheritance test"]]
-        [:div {:class (with-style keyed :paper)}]
-
-        [:div {:class (with-style keyed :sheet)}]
-
-        [:div {:class (with-style media-query-test)}
-         "Media query test"]]
-       #_[:div {:class (with-style some-style)}
-        "Regular maps"]
-       ])))
+      [container
+       [:div {:class (with-style flex-container)}
+        [header]]
+       ]
+      ;; TODO demo inheritance variants
+      ;; TODO demo state
+      ;; TODO demo media
+      ;; TODO demo keys
+      ;; TODO demo anon
+      )))
 
 (defn mount-root []
   (r/render [home-page] (.getElementById js/document "app")))
 
 (defn init! []
+  (herb/set-global-style! global-style)
   (mount-root))
 
 (init!)
