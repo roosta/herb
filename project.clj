d74b4ad0523b65321bbfaddd20d35607af850ba5
Daniel Berg <mail@roosta.sh>
2018-04-05 12:37:09 +0200

    Add debux stubs

--- project.clj
+++ project.clj
@@ -9,9 +9,10 @@
 
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.9.473"]
+                 [philoskim/debux-stubs "0.4.5"]
                  [garden "1.3.3"]]
 
-  :profiles {:dev {:dependencies [[philoskim/debux "0.4.3"]
+  :profiles {:dev {:dependencies [[philoskim/debux "0.4.5"]
                                   [etaoin "0.2.8-SNAPSHOT"]]
                    :plugins [[lein-doo "0.1.8"]]}}
 
