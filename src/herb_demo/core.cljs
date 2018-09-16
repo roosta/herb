2d504a3ebc04ef74a80cb7c322b1dbe00ae0711e
Daniel Berg <mail@roosta.sh>
2018-09-16 17:52:50 +0200

    Setup routing

--- src/herb_demo/core.cljs
+++ src/herb_demo/core.cljs
@@ -2,28 +2,51 @@
   (:require
    [herb.core :include-macros true :refer [<class defgroup <id global-style!]]
    [garden.selectors :as s]
+   [secretary.core :as secretary :include-macros true]
+   [accountant.core :as accountant]
    [garden.core :refer [css]]
    [garden.stylesheet :refer [at-media]]
-   [taoensso.tufte :as tufte :refer-macros [defnp p profiled profile]]
    [garden.core :refer [css]]
    [herb-demo.examples :refer [examples]]
    [garden.units :refer [px em]]
    [reagent.debug :as d]
    [reagent.core :as r]))
 
+(def global-style
+  [:body {:margin 0
+          :font-family ["Helvetica Neue" "Verdana" "Helvetica" "Arial" "sans-serif"]}])
+
+(defn home-page []
+  [:div
+   [:ul
+    [:li [:a {:href "/examples"}
+          "Examples"]]
+    [:li [:a {:href "/site"}
+           "Site"]]]])
+
+(def page (r/atom #'home-page))
+
+(secretary/defroute "/" []
+  (reset! page #'home-page))
+
+(secretary/defroute "/examples" []
+  (reset! page #'examples))
+
+(defn appframe []
+  [:div [@page]])
+
 (defn mount-root []
-  (r/render [examples] (.getElementById js/document "demo")))
+  (r/render [appframe] (.getElementById js/document "demo")))
 
 (defn init!
   []
-  (mount-root)
-  #_(global-style! (list [:body
-                        {:font-family ["Helvetica Neue" "Verdana" "Helvetica" "Arial" "sans-serif"]
-                         :max-width (px 600)
-                         :margin "0 auto"
-                         :padding-top (px 72)
-                         :-webkit-font-smoothing "antialiased"
-                         :font-size (em 1.125)
-                         :color "#333"
-                         :line-height (em 1.5)
-                         }])))
+  (global-style! global-style)
+  (accountant/configure-navigation!
+   {:nav-handler
+    (fn [path]
+      (secretary/dispatch! path))
+    :path-exists?
+    (fn [path]
+      (secretary/locate-route path))})
+  (accountant/dispatch-current!)
+  (mount-root))
