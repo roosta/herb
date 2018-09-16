dbec384ed57aaca53866ccdd8d49e876852eaed2
Daniel Berg <mail@roosta.sh>
2018-09-16 18:21:11 +0200

    Include site examples

---
+++ src/herb_demo/examples/intro.cljs
@@ -0,0 +1,16 @@
+(ns herb-demo.examples.intro
+  (:require
+   [herb.core :refer [<class <id]]))
+
+;; Define a function that returns a map containing styles.
+(defn example-style
+  []
+  {:background-color "#FF9999"
+   :height "50px"
+   :width "100%"})
+
+(defn example-component
+  []
+  ;; using either <class or <id, the macros both take a function, and returns a
+  ;; unique classname or id that we attach to our div here.
+  [:div {:class (<class example-style)}])
