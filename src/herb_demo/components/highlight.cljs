b7558f089194a7261fcf8097820c18be02aa08c2
Daniel Berg <mail@roosta.sh>
2018-09-19 18:22:11 +0200

    Include highlight

--- src/herb_demo/components/highlight.cljs
+++ src/herb_demo/components/highlight.cljs
@@ -1,6 +1,7 @@
 (ns herb-demo.components.highlight
   (:require [reagent.core :as r]
             [goog.object :as gobj]
+            [cljsjs.highlight]
             [cljsjs.react-highlight]))
 
 (def Highlight (r/adapt-react-class js/Highlight))
