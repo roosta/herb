7fcb8a3614ccf7aadb5d69b5f940da419346dfb2
Daniel Berg <mail@roosta.sh>
2018-11-13 18:29:14 +0100

    Refactor sidebar items

--- src/herb_demo/components/navigation.cljs
+++ src/herb_demo/components/navigation.cljs
@@ -2,6 +2,7 @@
   (:require [garden.units :refer [px rem]]
             [goog.events :as events]
             [debux.cs.core :as d :refer-macros [clog clogn dbg dbgn break]]
+            [clojure.string :as str]
             [herb-demo.easing :as easing]
             [garden.color :refer [rgba]]
             [goog.dom :as dom]
@@ -43,13 +44,37 @@
    :container {:padding (rem 1)}
    :row {:padding-bottom (rem 1)}})
 
-(defn a-style []
-  ^{:pseudo {:hover {:color "#3BABFF"}}}
-  {:color "white"})
+(defn nav-item-style [padding?]
+  ^{:pseudo {:hover {:color "#3BABFF"}}
+    :key padding?}
+  {:color "white"
+   :padding-left (if padding? (rem 1) 0)})
 
-(defn sub-a-style []
-  ^{:extend a-style}
-  {:padding-left (rem 1)})
+(defn nav-item
+  [[k v] index]
+  (let [sub? (> index (count items))
+        strindex (str index)]
+    [:div  {:class (<class sidebar-style :row)}
+     [:a {:href (str "#" (name k))}
+      [text {:class (<class nav-item-style sub?)
+             :color :white
+             :variant :a}
+       [:strong (if sub?
+                  (str (first strindex) "." (apply str (rest strindex)) ". ")
+                  (str index ". "))]
+       (:label v)]]]))
+
+(defn create-nav
+  [items index result]
+  (if (seq items)
+    (if-let [sub (-> items first val :sub)]
+      (recur (rest items)
+             (inc index)
+             (into result (create-nav sub (+ (* 10 (dec index)) 1) (list))))
+      (recur (rest items)
+             (inc index)
+             (conj result ^{:key index} [nav-item (first items) index])))
+    (reverse result)))
 
 (defn sidebar []
   (r/create-class
@@ -58,29 +83,7 @@
     (fn []
       [:section {:class (<class sidebar-style :root)}
        [:nav {:class (<class sidebar-style :container)}
-        (doall
-         (map (fn [[k v] index]
-                ^{:key (:label v)}
-                [:div
-                 [:div  {:class (<class sidebar-style :row)}
-                  [:a {:href (str "#" (name k))}
-                   [text {:class (<class a-style)
-                          :color :white
-                          :variant :a}
-                    [:strong (str index ". ")] (:label v)]]]
-                 (when-let [sub (:sub v)]
-                   [:div {:class (<class sidebar-style :row)}
-                    (map (fn [[k v] sub-index]
-                           ^{:key (:label v)}
-                           [:a {:class (<class sub-a-style)
-                                :href (str "#" (name k))}
-                            [text {:variant :a
-                                   :color :white}
-                             [:strong (str index "." sub-index " ")] (:label v)]])
-                         sub
-                         (map inc (range)))])])
-              items
-              (map inc (range))))]])}))
+        (create-nav items 1 (list))]])}))
 
 (defcssfn calc
   [& args]
