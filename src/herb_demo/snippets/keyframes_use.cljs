68198272bc6b323876c61d7965242e1d73e4ff05
Daniel Berg <mail@roosta.sh>
2018-11-30 19:30:03 +0100

    Add keyframes tutorial

---
+++ src/herb_demo/snippets/keyframes_use.cljs
@@ -0,0 +1,21 @@
+(ns herb-demo.snippets.keyframes-use
+  (:require
+   [garden.units :refer [px]]
+   [herb.core :refer-macros [<class defkeyframes]]))
+
+(defkeyframes pulse-animation
+  [:from {:opacity 1}]
+  [:to {:opacity 0}])
+
+;; In garden, if you use a single vector [arg1 arg1] you get a comma separated
+;; string, if you add a second vector is gets space separated
+(defn style
+  []
+  {:animation [[pulse-animation "2s" :infinite :alternate]]
+   :background-color "black"
+   :width (px 20)
+   :height (px 20)})
+
+(defn component
+  []
+  [:div {:class (<class style)}])
