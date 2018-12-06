45e3a58c9f4513c05e9cd2eb155e5f827c730426
Daniel Berg <mail@roosta.sh>
2018-12-06 16:02:34 +0100

    Add section about inline prefixes

--- src/herb_demo/tutorials/vendor_prefixes.cljs
+++ src/herb_demo/tutorials/vendor_prefixes.cljs
@@ -6,6 +6,7 @@
    [herb-demo.components.text :refer [text]]
    [herb-demo.snippets.vendor-init :as init]
    [herb-demo.snippets.vendor-local :as local]
+   [herb-demo.snippets.inline-prefixes :as inline]
    [herb.core :as herb :refer-macros [<class <id]]
    [reagent.core :as r])
   (:require-macros
@@ -15,12 +16,15 @@
 (defn main []
   (let [e1 (macros/example-src "vendor_init.cljs")
         e2 (macros/example-src "vendor_init.html")
-        e3 (macros/example-src "vendor_local.cljs")]
+        e3 (macros/example-src "vendor_local.cljs")
+        e4 (macros/example-src "inline_prefixes.cljs")
+        e5 (macros/example-src "inline_prefixes.html")]
     [paper {:id "vendor-prefixes"}
      [text {:variant :heading}
       "Vendor prefixes"]
-     [text "You can define vendor prefixes in two ways, the first one is by using
-      `herb.core/init!` and passing the options `:vendors` and `:auto-prefix`"]
+     [text "You can define vendor prefixes several ways, the first one is by
+      using `herb.core/init!` and passing the options `:vendors` and
+      `:auto-prefix`"]
      [code {:lang :clojure}
       e1]
      [text "By using auto-prefix all selected properties are prefixed with the
@@ -32,4 +36,10 @@
       e3]
      [text "Bear in mind that any property in a stylemap marked with prefix are
      going to be prefixed, so if I had `:color \"red\"` in there that would be
-     prefixed as well."]]))
+     prefixed as well."]
+     [text "You can also inline the prefixes using garden syntax: "]
+     [code {:lang :clojure}
+      e4]
+     [code {:lang :html}
+      e5
+      ]]))
