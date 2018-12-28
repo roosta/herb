95a7d0453acd4c3824b52abe2409bdb036ad053a
Daniel Berg <mail@roosta.sh>
2018-12-28 16:03:26 +0100

    Upgrade deps

--- site/project.clj
+++ site/project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.9.0"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
                  [ring-server "0.5.0"]
                  [reagent "0.8.1"]
                  [reagent-utils "0.3.1"]
@@ -23,7 +23,7 @@
 
   :plugins [[lein-environ "1.1.0"]
             [lein-cljsbuild "1.1.7"]
-            [lein-asset-minifier "0.2.7"
+            [lein-asset-minifier "0.4.5"
              :exclusions [org.clojure/clojure]]]
 
   :ring {:handler site.handler/app
@@ -73,12 +73,12 @@
                                   [ring/ring-mock "0.3.2"]
                                   [ring/ring-devel "1.7.1"]
                                   [pjstadig/humane-test-output "0.9.0"]
-                                  [figwheel-sidecar "0.5.17"]
-                                  [nrepl "0.4.5"]]
+                                  [figwheel-sidecar "0.5.18"]
+                                  [nrepl "0.5.3"]]
                    :source-paths ["env/dev"]
                    :injections [(require 'pjstadig.humane-test-output)
                                 (pjstadig.humane-test-output/activate!)]
-                   :plugins [[lein-figwheel "0.5.17"]]
+                   :plugins [[lein-figwheel "0.5.18"]]
                    :env {:dev true}}
 
              :uberjar {:hooks [minify-assets.plugin/hooks]
