b99050802473420d8f480b00d6277f6d22a954e5
Daniel Berg <mail@roosta.sh>
2019-02-26 18:02:46 +0100

    Upgrade deps

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :plugins [[lein-cljsbuild "1.1.7"]
-            [lein-codox "0.10.5"]]
+            [lein-codox "0.10.6"]]
 
   :source-paths ["src"]
 
@@ -22,20 +22,20 @@
   :jar-exclusions [#"(?:^|\/)demo\/" #"(?:^|\/)public\/"]
 
   :dependencies [[org.clojure/clojure "1.10.0" :scope "provided"]
-                 [org.clojure/clojurescript "1.10.439" :scope "provided"]
+                 [org.clojure/clojurescript "1.10.520" :scope "provided"]
                  [garden "1.3.6"]]
 
   :profiles {:dev {:dependencies [[binaryage/devtools "0.9.10"]
                                   [org.clojure/test.check "0.10.0-alpha3"]
                                   [figwheel "0.5.18"]
-                                  [philoskim/debux "0.5.2"]
+                                  [philoskim/debux "0.5.5"]
                                   [reagent "0.8.1"]
                                   [com.taoensso/tufte "2.0.1"]
                                   [figwheel-sidecar "0.5.18"]
-                                  [etaoin "0.2.9"]]
+                                  [etaoin "0.3.2"]]
                    :plugins [[lein-figwheel "0.5.18"]]
                    :source-paths ["test" "demo"]}
-             :dev-cider {:dependencies [[cider/piggieback "0.3.10"]]
+             :dev-cider {:dependencies [[cider/piggieback "0.4.0"]]
                          :figwheel {:nrepl-middleware [cider.piggieback/wrap-cljs-repl]}}}
 
   :cljsbuild {:builds [{:id "demo"
