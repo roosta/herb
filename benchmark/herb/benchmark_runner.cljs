8ba7ed69a3af9972de88fce44574f2078110c97e
Daniel Berg <mail@roosta.sh>
2019-08-07 14:45:02 +0200

    bind number of runs

--- benchmark/herb/benchmark_runner.cljs
+++ benchmark/herb/benchmark_runner.cljs
@@ -36,57 +36,57 @@
 
 (defn -main [& args]
 
-  (println "<class macro with arguments")
-  (let [runs 1000
-        start (.getTime (js/Date.))
-        _ (dotimes [n runs]
+  (let [runs 1000]
+    (println "<class macro with arguments")
+    (let [start (.getTime (js/Date.))
+          _ (dotimes [n runs]
               (<class function-with-args n))
-        end (.getTime (js/Date.))
-        elapsed (- end start)]
-    (println (str runs " runs, " elapsed " msecs")))
-  (println)
+          end (.getTime (js/Date.))
+          elapsed (- end start)]
+      (println (str runs " runs, " elapsed " msecs")))
+    (println)
 
-  (println "<class macro")
-  (simple-benchmark [] (<class extended-style) 10000)
-  (println)
+    (println "<class macro")
+    (simple-benchmark [] (<class extended-style) runs)
+    (println)
 
-  (println "impl/get-name")
-  (simple-benchmark [] (#'impl/get-name extended-style "perf.ns") 10000)
-  (println)
+    (println "impl/get-name")
+    (simple-benchmark [] (#'impl/get-name extended-style "perf.ns") runs)
+    (println)
 
-  (println "impl/create-data-string")
-  (simple-benchmark [] (#'impl/create-data-string extended-style "perf/test/ns") 10000)
-  (println)
+    (println "impl/create-data-string")
+    (simple-benchmark [] (#'impl/create-data-string extended-style "perf/test/ns") runs)
+    (println)
 
-  (println "impl/compose-selector")
-  (simple-benchmark [] (#'impl/compose-selector "perf/test/ns" "1234567") 10000)
-  (println)
+    (println "impl/compose-selector")
+    (simple-benchmark [] (#'impl/compose-selector "perf/test/ns" "1234567") runs)
+    (println)
 
-  (println "impl/prepare-data/extract-meta")
-  (simple-benchmark [] (#'impl/prepare-data resolved-styles) 10000)
-  (println)
+    (println "impl/prepare-data/extract-meta")
+    (simple-benchmark [] (#'impl/prepare-data resolved-styles) runs)
+    (println)
 
-  (println "impl/extract-extended-styles/resolve-style-fns")
-  (simple-benchmark [] (#'impl/extract-extended-styles [extended-style]) 10000)
+    (println "impl/extract-extended-styles/resolve-style-fns")
+    (simple-benchmark [] (#'impl/extract-extended-styles [extended-style]) runs)
 
-  (println "append to DOM")
-  (simple-benchmark [element (.createElement js/document "style")]
-                    (dom/append element ".test {color: \"red\"}")
-                    10000)
-  (println)
+    (println "append to DOM")
+    (simple-benchmark [element (.createElement js/document "style")]
+                      (dom/append element ".test {color: \"red\"}")
+                      runs)
+    (println)
 
-  (println "Create css string using garden")
-  (simple-benchmark [] (css [:.classname {:width (px 100)
-                                          :height (px 100)
-                                          :background-color "magenta"
-                                          :border-radius "5px"}])
-                    10000)
-  (println)
+    (println "Create css string using garden")
+    (simple-benchmark [] (css [:.classname {:width (px 100)
+                                            :height (px 100)
+                                            :background-color "magenta"
+                                            :border-radius "5px"}])
+                      runs)
+    (println)
 
-  (println "creating media queries using garden")
-  (simple-benchmark [] (at-media {:max-width "256px"}
-                                 [:.classname {:width (px 100)
-                                               :height (px 100)
-                                               :background-color "magenta"
-                                               :border-radius "5px"}])
-                    10000))
+    (println "creating media queries using garden")
+    (simple-benchmark [] (at-media {:max-width "256px"}
+                                   [:.classname {:width (px 100)
+                                                 :height (px 100)
+                                                 :background-color "magenta"
+                                                 :border-radius "5px"}])
+                      runs)))
